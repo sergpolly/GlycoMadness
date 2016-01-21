b6f72dd20805a0646f0278a7d2b1a5110017da4f
sergpolly <sergpolly@gmail.com>
2016-01-21 14:21:33 -0500

    run all pipelines with reports ...

---
+++ pipeline_original_output_before_dec25.sh
@@ -0,0 +1,49 @@
+INPUT="../raw_data/New_files_to_analyze/original_input_before_dec25"
+OUTPUT="../Outputs/original_output_before_dec25"
+
+
+# goto the GlycoMadness folder ...
+# and execute from there:
+
+echo "Backing up required protein sequences from UniProt ..."
+# get needed protein sequences from uniprot and store em locally ...
+# kinda like that:
+# python  backup_uids.py  ../input_peptides.xls  backup_uids_output.fasta
+python backup_uids.py \
+	"$INPUT"/peptides.xls \
+	"$OUTPUT"/backup_uids_output.fasta
+
+
+echo "Unique peptides identification and clean up ..."
+# then do some clean up in the 'proteins' file using the same proteins file and proteins backup
+# python  resolve_peptide_ambiguity.py  ../input_peptides.xls  backup_uids_output.fasta  uniq_peptides_catalog.sv
+python resolve_peptide_ambiguity.py \
+	"$INPUT"/peptides.xls \
+	"$OUTPUT"/backup_uids_output.fasta \
+	"$OUTPUT"/uniq_peptides_catalog.csv
+
+
+
+echo "Finally, extracting G-sites ..."
+# finally extract all gsites and make final output ...
+# python  gsites_catalog.py  ../peptides.xls  ../specs.xls  backup_uids_output.fasta  uniq_peptides_catalog.csv gsites_antology.csv
+python  gsites_catalog.py \
+	"$INPUT"/peptides.xls \
+	"$INPUT"/specs.xls \
+	"$OUTPUT"/backup_uids_output.fasta \
+	"$OUTPUT"/uniq_peptides_catalog.csv \
+	"$OUTPUT"/gsites_antology.csv
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ run_all.sh
@@ -0,0 +1,24 @@
+
+sh pipeline_output_01212016.sh > ../Outputs/output_01212016/report.txt
+
+
+sh pipeline_original_output_before_dec25.sh > ../Outputs/original_output_before_dec25/report.txt
+
+
+sh pipeline_new_output_small_111815.sh > ../Outputs/new_output_small_111815/report.txt
+
+
+sh pipeline_new_output_large_121515.sh > ../Outputs/new_output_large_121515/report.txt
+
+
+
+
+
+
+
+
+
+
+
+
+
